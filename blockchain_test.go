211546864e910c4f55867b54ccafce0487e3ed3e
wuyang <ocean5tech@hotmail.com>
2023-04-17 23:57:41 +0800

    test

--- blockchain_test.go
+++ blockchain_test.go
@@ -6,10 +6,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// func TestBlockNew(t *testing.T) {
-// 	nonce := 1
-// 	prevHash := "test"
-// 	newb := NewBlock(nonce, prevHash)
-// 	assert.Equal(t, newb.nonce, nonce)
-// 	assert.Equal(t, newb.previousHash, prevHash)
-// }
+func TestBlockNew(t *testing.T) {
+	nonce := 1
+	prevHash := "test"
+	newb := NewBlock(nonce, prevHash)
+	assert.Equal(t, newb.nonce, nonce)
+	assert.Equal(t, newb.previousHash, prevHash)
+}
